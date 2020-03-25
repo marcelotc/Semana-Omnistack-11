1cab63ee896d08a6ba60d3bf74a91ca74afcf272
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-03-25 16:23:09 -0300

    profile and session routes

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -11,12 +11,32 @@ module.exports = {
             value,
             ong_id
         });
-        return response.json({"message": 'salvo'});
+        return response.json({ "message": 'salvo' });
     },
 
     async index(request, response) {
         const incidents = await connection('incidents').select('*');
 
         return response.json(incidents);
+    },
+
+    async delete(request, response) {
+        const { id } = request.params;
+        const ong_id = request.headers.authorization;
+
+        const incidents = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first();
+
+        if (incidents.ong_id !== ong_id) {
+            return response.status(401).json({ error: 'Operation not permitted' });
+        }
+
+        await connection('incidents').where('id', id).delete();
+
+        return response.status(204).send();
     }
+
+    
 };
