355dde09df590fd7ca970c3254ee5c35986ceb42
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-03-24 23:59:23 -0300

    knex connection and basic routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,31 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection')
 
 const routes = express.Router();
 
-routes.get('/users', (request, response) => {
+routes.get('/ongs', async (request, response) => {
+    const ongs = await connection('ongs').select('*');
 
-    const body = request.body
-    console.log(body)
+    return response.json(ongs);
+});
 
-    return response.json({
-        evento: 'Semana Omnistack 11.0',
-        aluno: 'Marcelo Cortes'
+routes.post('/ongs', async (request, response) => {
+
+    const { name, email, whatsapp, city, uf } = request.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
     })
+
+    return response.json({ id });
 })
 
 module.exports = routes;
